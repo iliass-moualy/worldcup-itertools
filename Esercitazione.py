1eb8cdc93e3de7951c81d27735f52c98608cc07a
iliass-moualy <iliassmoualy99@gmail.com>
2021-03-31 09:55:08 +0200

    punto 1

---
+++ Esercitazione.py
@@ -0,0 +1,22 @@
+import csv
+
+
+
+def get_worldcup_matches(csv_filename):
+    with open(csv_filename, "r", encoding="utf8") as match_records:
+        for match_record in csv.reader(match_records):
+            yield match_record
+
+def filtering(row):
+    return row[5] == "FIFA World Cup" #Filters per Wolrd CUp games only
+
+
+iter_matches = iter(get_worldcup_matches("results.csv"))
+
+iter_matches = filter(filtering, iter_matches)
+#next(iter_matches)  # Skipping the column names
+
+
+
+for row in iter_matches:
+    print(row)
