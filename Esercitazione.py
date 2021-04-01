5672792f936a482e9eaa84729c8a9fe7e80308ea
iliass-moualy <iliassmoualy99@gmail.com>
2021-04-01 11:06:01 +0200

    mprofiler

--- Esercitazione.py
+++ Esercitazione.py
@@ -1,35 +1,40 @@
 import csv
 import itertools as it
+from memory_profiler import profile
+
+@profile
+def main_func():
+    def get_worldcup_matches(csv_filename):
+        with open(csv_filename, "r", encoding="utf8") as match_records:
+            for match_record in csv.reader(match_records):
+                yield match_record
+
+    def filtering(row):
+        return row[5] == "FIFA World Cup" #Filters per Wolrd Cup games only
 
 
-def get_worldcup_matches(csv_filename):
-    with open(csv_filename, "r", encoding="utf8") as match_records:
-        for match_record in csv.reader(match_records):
-            yield match_record
+    iter_matches = iter(get_worldcup_matches("results.csv"))
 
-def filtering(row):
-    return row[5] == "FIFA World Cup" #Filters per Wolrd Cup games only
+    iter_matches = filter(filtering, iter_matches)
+    #next(iter_matches)  # Skipping the column names
 
 
-iter_matches = iter(get_worldcup_matches("results.csv"))
+    dictionaries = []
+    listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
 
-iter_matches = filter(filtering, iter_matches)
-#next(iter_matches)  # Skipping the column names
-
-
-dictionaries = []
-listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
-
-for row in iter_matches:
-    dictionaries.append(dict(zip(listOfKeys,row)))
+    for row in iter_matches:
+        dictionaries.append(dict(zip(listOfKeys,row)))
 
 
-print(dictionaries)
+    print(dictionaries)
 
-italy_goal_count = 0
+    italy_goal_count = 0
 
-italy_goal_count = sum(int(m_dict['home score']) for m_dict in dictionaries if(m_dict['home team'] == "Italy"))
+    italy_goal_count = sum(int(m_dict['home score']) for m_dict in dictionaries if(m_dict['home team'] == "Italy"))
 
-italy_goal_count += sum(int(m_dict['away score']) for m_dict in dictionaries if(m_dict['away team'] == "Italy"))
-    
-print(italy_goal_count)
+    italy_goal_count += sum(int(m_dict['away score']) for m_dict in dictionaries if(m_dict['away team'] == "Italy"))
+        
+    print(italy_goal_count)
+
+if __name__ == "__main__":
+    main_func()
