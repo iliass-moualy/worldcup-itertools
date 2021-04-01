95a8df6f03152c6abfbe5062450de932cdaf89da
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2021-04-01 10:45:40 +0200

    refactoring

--- Esercitazione.py
+++ Esercitazione.py
@@ -7,30 +7,30 @@ def get_worldcup_matches(csv_filename):
 def filtering(row):
     return row[5] == "FIFA World Cup" #Filters per Wolrd Cup games only
 
-if (__name__ == "__main__"):
-    iter_matches = iter(get_worldcup_matches("results.csv"))
 
-    iter_matches = filter(filtering, iter_matches)
-    #next(iter_matches)  # Skipping the column names
+iter_matches = iter(get_worldcup_matches("results.csv"))
 
-    dictionaries = []
-    listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
+iter_matches = filter(filtering, iter_matches)
+#next(iter_matches)  # Skipping the column names
 
-
-    for row in iter_matches:
-        dictionaries.append(dict(zip(listOfKeys,row)))
-        #print(row)
+dictionaries = []
+listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
 
 
+for row in iter_matches:
+    dictionaries.append(dict(zip(listOfKeys,row)))
+    #print(row)
 
-    print(len(dictionaries))
 
-    italy_goal_count = 0
 
-    for m_dict in dictionaries:
-        if(m_dict['home team'] == "Italy"):
-            italy_goal_count += int(m_dict['home score'])
-        elif(m_dict['away team'] == "Italy"):
-            italy_goal_count += int(m_dict['away score'])
-            
-    print(italy_goal_count)
+print(len(dictionaries))
+
+italy_goal_count = 0
+
+for m_dict in dictionaries:
+    if(m_dict['home team'] == "Italy"):
+        italy_goal_count += int(m_dict['home score'])
+    elif(m_dict['away team'] == "Italy"):
+        italy_goal_count += int(m_dict['away score'])
+        
+print(italy_goal_count)
