e5296a25baf636041375239462645ea7db3804c3
iliass-moualy <iliassmoualy99@gmail.com>
2021-04-06 15:34:05 +0200

    senza main

--- Esercitazione.py
+++ Esercitazione.py
@@ -3,50 +3,46 @@ import itertools as it
 from memory_profiler import profile
 
 
-def main_func():
+#Generatore per estrarre le partite riga per riga
+def get_worldcup_matches(csv_filename):
+    with open(csv_filename, "r", encoding="utf8") as match_records:
+        for match_record in csv.reader(match_records):
+            yield match_record
 
-    #Generatore per estrarre le partite riga per riga
-    def get_worldcup_matches(csv_filename):
-        with open(csv_filename, "r", encoding="utf8") as match_records:
-            for match_record in csv.reader(match_records):
-                yield match_record
-
-    #Funzione per filtrare filtrare solo le partite dei mondiali
-    def filtering(row):
-        return row[5] == "FIFA World Cup" 
+#Funzione per filtrare filtrare solo le partite dei mondiali
+def filtering(row):
+    return row[5] == "FIFA World Cup" 
 
 
-    #iteratore con le partite
-    iter_matches = iter(get_worldcup_matches("results.csv"))
+#iteratore con le partite
+iter_matches = iter(get_worldcup_matches("results.csv"))
 
-    #iteratore con le partite dei mondiali
-    iter_matches = filter(filtering, iter_matches)
+#iteratore con le partite dei mondiali
+iter_matches = filter(filtering, iter_matches)
+
+
+next(iter_matches)  # Skipping the column names
+
+
+
+#Creazione dizionari e lista di dizionari 
+dictionaries = []
+listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
+
+for row in iter_matches:
+    dictionaries.append(dict(zip(listOfKeys,row)))
+
+
+#print(dictionaries)
+
+
+#Conteggio gol fatti dall'italia nei campionati mondiali tramite generator expressions
+
+italy_goal_count = 0
+
+italy_goal_count = sum(int(m_dict['home score']) for m_dict in dictionaries if(m_dict['home team'] == "Italy"))
+
+italy_goal_count += sum(int(m_dict['away score']) for m_dict in dictionaries if(m_dict['away team'] == "Italy"))
     
+#print(italy_goal_count)
 
-    next(iter_matches)  # Skipping the column names
-
-
-
-    #Creazione dizionari e lista di dizionari 
-    dictionaries = []
-    listOfKeys=["date","home team","away team","home score","away score","tournament","city","country","neutral"]
-
-    for row in iter_matches:
-        dictionaries.append(dict(zip(listOfKeys,row)))
-
-
-    #print(dictionaries)
-
-
-    #Conteggio gol fatti dall'italia nei campionati mondiali tramite generator expressions
-
-    italy_goal_count = 0
-
-    italy_goal_count = sum(int(m_dict['home score']) for m_dict in dictionaries if(m_dict['home team'] == "Italy"))
-
-    italy_goal_count += sum(int(m_dict['away score']) for m_dict in dictionaries if(m_dict['away team'] == "Italy"))
-        
-    #print(italy_goal_count)
-
-if __name__ == "__main__":
-    main_func()
--- non-generators-version.py
+++ non-generators-version.py
@@ -2,56 +2,52 @@ import csv
 from memory_profiler import profile
 
 
-def main_func():
 
-    fields = []
-    matches = []
-    #apertura del csv ed estrazione delle righe in lista matches
-    with open("results.csv", 'r', encoding="utf8") as csvfile:
-        csvreader = csv.reader(csvfile)
+fields = []
+matches = []
+#apertura del csv ed estrazione delle righe in lista matches
+with open("results.csv", 'r', encoding="utf8") as csvfile:
+    csvreader = csv.reader(csvfile)
 
-        fields = next(csvreader)
+    fields = next(csvreader)
 
-        for match in csvreader:
-            matches.append(match)
-    
-    #Funzinoe per filtrare partite dei mondiali
-    def filtering(row):
-        return row[5] == "FIFA World Cup" #Filters per Wolrd Cup games only
+    for match in csvreader:
+        matches.append(match)
 
-    #Filtrare partite
-    worldcup_matches = list(filter(filtering, matches))
+#Funzinoe per filtrare partite dei mondiali
+def filtering(row):
+    return row[5] == "FIFA World Cup" #Filters per Wolrd Cup games only
 
-
-    #Lista di dizionari con una partita ciascuno
-    dictionaries = []
-
-    for match in worldcup_matches:
-        dictionaries.append({
-            "date" : match[0],
-            "home team" : match[1],
-            "away team" : match[2],
-            "home score" : match[3],
-            "away score" : match[4],
-            "tournament" : match[5],
-            "city" : match[6],
-            "country" : match[7],
-            "neutral" : match[8],
-        })
+#Filtrare partite
+worldcup_matches = list(filter(filtering, matches))
 
 
+#Lista di dizionari con una partita ciascuno
+dictionaries = []
 
-    #conteggio gol dell'italia iterando tutta la lista di dizionari#
-    italy_goals_count = 0
+for match in worldcup_matches:
+    dictionaries.append({
+        "date" : match[0],
+        "home team" : match[1],
+        "away team" : match[2],
+        "home score" : match[3],
+        "away score" : match[4],
+        "tournament" : match[5],
+        "city" : match[6],
+        "country" : match[7],
+        "neutral" : match[8],
+    })
 
-    for match in dictionaries:
-        if(match['home team'] == "Italy"):
-            italy_goals_count += int(match['home score'])
-        elif(match['away team'] == "Italy"):
-            italy_goals_count += int(match['away score'])
-
-    print(italy_goals_count)
 
 
-if __name__ == "__main__":
-    main_func()
+#conteggio gol dell'italia iterando tutta la lista di dizionari#
+italy_goals_count = 0
+
+for match in dictionaries:
+    if(match['home team'] == "Italy"):
+        italy_goals_count += int(match['home score'])
+    elif(match['away team'] == "Italy"):
+        italy_goals_count += int(match['away score'])
+
+print(italy_goals_count)
+
